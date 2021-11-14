9a157bbb37b7ae13b6da0c48af3fdb1a21a4444e
jwstegemann <jwstegemann@gmail.com>
2021-11-14 20:28:02 +0100

    working memory test

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("dev.fritz2:core:0.13")
+                implementation("dev.fritz2:core:0.14-SNAPSHOT")
                 // see https://components.fritz2.dev/
                 // implementation("dev.fritz2:components:0.13")
             }
